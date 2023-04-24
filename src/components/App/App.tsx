a0fb822c08191656c0caea65af06ad82fe9a4e8b
ValentinDeloffre <v.deloffre9@gmail.com>
2023-04-24 13:18:14 +0200

    version 1 ok

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,9 +4,11 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h2>Bienvenue sur le météo widget de Solene</h2>
-      <WidgetMeteo city="Paris" />
-      <WidgetMeteo city="Savines-le-Lac" />
+      <h2>Bienvenue sur la météo widget de Valentin</h2>
+      <WidgetMeteo city="Paris" zipcode="75001" />
+      <WidgetMeteo city="Marseille" zipcode="13000" />
+      <WidgetMeteo city="Nice" zipcode="06300" />
+      <WidgetMeteo city="Avesnes-Le-Sec" zipcode="59296" />
     </div>
   )
 }
--- src/components/WidgetMeteo/WidgetMeteo.tsx
+++ src/components/WidgetMeteo/WidgetMeteo.tsx
@@ -1,11 +1,46 @@
-function WidgetMeteo() {
-    return (
-      <div className="meteo">
-        <div className="meteo-city">Paris</div>
-        <div className="meteo-zipcode">75001</div>
-        <div className="meteo-temperature">10°C</div>
-      </div>
-    )
-  }
-  
-  export default WidgetMeteo;
+import { useEffect, useState } from 'react';
+import './WidgetMeteo.scss';
+import axios from 'axios';
+
+
+interface WidgetMeteoProps {
+  city: string;
+  zipcode: string;
+}
+
+
+function WidgetMeteo({ city, zipcode }: WidgetMeteoProps) {
+
+  const [temperature, setTemperature] = useState(0);
+  const [icon, setIcon] = useState('04d');
+
+  const fetchTemperature = async () => {
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},fr&appid=${API_KEY}&units=metric`);
+    // console.log(result);
+    setTemperature(Math.round(result.data.main.temp));
+    setIcon(result.data.weather[0].icon)
+  };
+
+  useEffect(
+    () => {
+      fetchTemperature();
+    },
+    [zipcode]
+  );
+
+  return (
+    <div className="meteo">
+    <div>
+      <div className="meteo-city">{city}</div>
+      <div className="meteo-zipcode">{zipcode}</div>
+      <div className="meteo-temperature">{temperature}°C</div>
+    </div>
+    <div>
+      <img src={`https://openweathermap.org/img/wn/${icon}.png`} />
+    </div>
+  </div>
+  );
+}
+
+export default WidgetMeteo;
